c528b99c442bc07c185c459234f3588a0706b685
Kveini77 <kveini77@gmail.com>
2024-02-23 14:50:00 +0600

    Roman1

---
+++ class1.py
@@ -0,0 +1,6 @@
+class Hello:
+    def __init__(self, str):
+        self.str = str
+
+
+class Hi(Hello):...
--- task.py
+++ task.py
@@ -1,3 +1,6 @@
+from class1 import Hi
 from art import tprint
 tprint("PYTHON")
 
+hi = Hi("HELLO")
+tprint(hi.str)
